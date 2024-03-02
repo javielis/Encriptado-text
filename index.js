f9c4858cd5dc3b55b5eecf8c4b7645d3133428bc
javielis <javielisrondon@gmail.com>
2024-03-01 23:57:41 -0500

    Funcion de desencriptar un texto

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function encriptar(){
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
         
-    if (document.getElementById("texto").value.length != 0) {
+    if (texto.length != 0) {
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con exito";
         parrafo.textContent = "";
@@ -24,5 +24,29 @@ function encriptar(){
     }
    
 
+}
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje")
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
 
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/ines/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con exito";
+        parrafo.textContent = "";
+        muñeco.src = "./imagenes/desencriptado.jpg";
+    } else {
+        muñeco.src = "/imagenes/muñeco.png";
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert ("Debes ingresar algun texto");
+    }
 }
